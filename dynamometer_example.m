2f2f51b0d2bc179eec74e083df4398b10c6ebdd7
Lionel Rigoux <lionel.rigoux@sf.mpg.de>
2025-04-03 15:42:52 +0200

    clean example

--- dynamometer_example.m
+++ dynamometer_example.m
@@ -1,31 +1,29 @@
 % minimal example
 
-
-
-
 % trial duration
 T  = 10 ; % in s
 dt = 0.05 ; % display refresh lag
 
 % display
 figure; hold on;
-xlim([0 T])
-ylim([-5 700])
+xlim ([0 T])
+ylim ([-5 700])
 
 % create dynamometer object
-d=dynamometer;
+d = dynamometer;
 
 % start recording
 d.start;
 tic
 
 % time loop
-for i=1:round(T/dt) 
-    pause(dt);
+for i = 1 : round (T / dt) 
     % update internal buffer and return last value
-    f = d.read; 
+    f = d.read;     
     % display
-    plot(toc,f,'r.'); 
+    plot (toc, f, 'r.'); 
+    % wait for next frme
+    pause (dt);
 end
 
 % stop recording and get the buffer
